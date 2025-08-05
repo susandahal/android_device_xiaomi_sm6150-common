f0e85c2d8c0324298bd1a4241212da7d32ebe2a3
susandahal <susandahal53@gmail.com>
2025-08-05 20:58:38 +0545

    YAAP common

--- sm6150.mk
+++ sm6150.mk
@@ -205,16 +205,16 @@ PRODUCT_ENABLE_UFFD_GC := true
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
 # Lineage Health
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
-$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
-$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+#$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+#$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+#$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
+#$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
 
 # LiveDisplay
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.xiaomi_sm6150
 
 # Media
